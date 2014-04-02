82c17bfc9d5827c2af00c21a6ddac935b91a24a6
Jason Narog <webtech@saybrook.edu>
2014-04-02 12:12:45 -0700

    changed how functions grabs stylesheet directory to allow for easier overrides for child theme

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ include (TEMPLATEPATH . '/functions/htmlmods.php');
 
 /* Styles */
 function lazy_grid_styles() {
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css' );
+	wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/css/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'lazy_grid_styles' );
 
